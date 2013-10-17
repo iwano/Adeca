33057e8d4aba2b6e992c78ef5a0b75998b18a637
Ivan Calderon <iwano@hotmail.com>
2013-10-16 20:38:30 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adeca::Application.config.secret_key_base = 'd05f08027b01092aaa0220e8a0a6c445753f11199dfbcb0b3992ff23cafb613adeae3d4789f9ad8b434407ed28fc83975e05c3848f31139fd11803226c404c3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Adeca::Application.config.session_store :cookie_store, key: '_adeca_session'
