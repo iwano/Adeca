2250f49efc1f3243c6583d578a013295deaccf96
Ivan Calderon <iwano@hotmail.com>
2014-01-18 19:41:15 -0600

    Remove solr

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,12 +2,6 @@ class Property < ActiveRecord::Base
   has_many :photos
   accepts_nested_attributes_for :photos, :allow_destroy => true
 
-  searchable do
-    text    :description
-    string  :address
-    string  :property_type
-  end
-
   rails_admin do
     configure :photos do
       label 'Property photos'
