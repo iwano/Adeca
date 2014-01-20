f2fb687dbbe04e6935cb5d3d7f8eef4f45850825
Ivan Calderon <iwano@hotmail.com>
2014-01-19 23:00:45 -0600

    Improve overall styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def current_page_with_param(controller, action='', param)
+    'active' if action?(action) && controller?(controller) && params[:static_page] == param
+  end
+  def current_page(controller, action='' )
+    'active' if action?(action) && controller?(controller)
+  end
+
+  def controller?(*controller)
+    controller.include?(params[:controller])
+  end
+
+  def action?(*action)
+    action.include?(params[:action])
+  end
 end
