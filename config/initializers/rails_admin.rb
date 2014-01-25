46ec93e6893736ed17154bffc13e3dea9dc92d91
Ivan Calderon <iwano@hotmail.com>
2014-01-25 13:20:44 -0600

    Add locales to model's attributes

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -15,44 +15,45 @@ RailsAdmin.config do |config|
   config.current_user_method { current_admin } # auto-generated
 
   config.model Photo do
-    field :photo, :carrierwave
+    field :photo, :carrierwave do
+      label I18n.t('active_record.attributes.photo.photo')
+    end
   end
 
-  # If you want to track changes on your models:
-  # config.audit_with :history, 'Admin'
+  config.model Property do
+    field :property_type do
+      label I18n.t('active_record.attributes.property.property_type')
+    end
+    field :city do
+      label I18n.t('active_record.attributes.property.city')
+    end
+    field :address do
+      label I18n.t('active_record.attributes.property.address')
+    end
+    field :sale_price do
+      label I18n.t('active_record.attributes.property.sale_price')
+      label 'Precio'
+    end
+    field :description do
+      label I18n.t('active_record.attributes.property.description')
+    end
+    field :photos do
+      label I18n.t('active_record.attributes.property.photos')
+    end
+    group :default do
+      help I18n.t(:property_help)
+    end
+  end
 
-  # Or with a PaperTrail: (you need to install it first)
-  # config.audit_with :paper_trail, 'Admin'
-
-  # Display empty fields in show views:
-  # config.compact_show_view = false
-
-  # Number of default rows per-page:
-  # config.default_items_per_page = 20
-
-  # Exclude specific models (keep the others):
-  # config.excluded_models = []
-
-  # Include specific models (exclude the others):
-  # config.included_models = []
-
-  # Label methods for model instances:
-  # config.label_methods << :description # Default is [:name, :title]
-
-
-  ################  Model configuration  ################
-
-  # Each model configuration can alternatively:
-  #   - stay here in a `config.model 'ModelName' do ... end` block
-  #   - go in the model definition file in a `rails_admin do ... end` block
-
-  # This is your choice to make:
-  #   - This initializer is loaded once at startup (modifications will show up when restarting the application) but all RailsAdmin configuration would stay in one place.
-  #   - Models are reloaded at each request in development mode (when modified), which may smooth your RailsAdmin development workflow.
-
-
-  # Now you probably need to tour the wiki a bit: https://github.com/sferik/rails_admin/wiki
-  # Anyway, here is how RailsAdmin saw your application's models when you ran the initializer:
-
-
+  config.model Admin do
+    field :email do
+      label I18n.t('active_record.attributes.admin.email')
+    end
+    field :password do
+      label I18n.t('active_record.attributes.admin.password')
+    end
+    field :password_confirmation do
+      label I18n.t('active_record.attributes.admin.password_confirmation')
+    end
+  end
 end
